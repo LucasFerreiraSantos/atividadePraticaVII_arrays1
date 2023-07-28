3101fdd836c4e69d8f36a01ea85e817c85e9bfc8
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 18:21:47 -0300

    iniciado o repositório

---
+++ exer2/index.js
@@ -0,0 +1,7 @@
+
+
+// 2. Criar um array e percorrê-lo utilizando o loop for of
+// a. Crie um array com o nome "numeros" e adicione 5 números de
+// sua escolha
+// b. Utilize o loop for of para percorrer o array e exibir a soma dos
+// números na tela
